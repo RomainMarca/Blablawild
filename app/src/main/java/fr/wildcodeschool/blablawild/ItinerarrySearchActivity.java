0338321736eeab98258d97ce84fff5c0b17784f3
Romain Marca <rmarca@hotmail.fr>
2018-09-17 14:27:37 +0200

    adding DatePicker quest

--- app/src/main/java/fr/wildcodeschool/blablawild/ItinerarrySearchActivity.java
+++ app/src/main/java/fr/wildcodeschool/blablawild/ItinerarrySearchActivity.java
@@ -1,18 +1,26 @@
 package fr.wildcodeschool.blablawild;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
 
 public class ItinerarrySearchActivity extends AppCompatActivity {
 
     public static String EXTRA_DEPART = "EXTRA_DEPART";
     public static String EXTRA_NAME = "EXTRA_NAME";
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,7 +31,6 @@ public class ItinerarrySearchActivity extends AppCompatActivity {
         TextView textDepart = findViewById(R.id.edit_departure);
         TextView textDesti = findViewById(R.id.text_destination);
         TextView textDate = findViewById(R.id.text_date);
-        EditText editDate = findViewById(R.id.edit_date);
         Button btnSearch = findViewById(R.id.button_search);
 
 
@@ -49,7 +56,45 @@ public class ItinerarrySearchActivity extends AppCompatActivity {
                 }
 
         });
+
+
+        final Calendar myCalendar = Calendar.getInstance();
+
+        final EditText edittext= (EditText) findViewById(R.id.Birthday);
+        final DatePickerDialog.OnDateSetListener date = new DatePickerDialog.OnDateSetListener() {
+
+            @Override
+            public void onDateSet(DatePicker view, int year, int monthOfYear,
+                                  int dayOfMonth) {
+                // TODO Auto-generated method stub
+                myCalendar.set(Calendar.YEAR, year);
+                myCalendar.set(Calendar.MONTH, monthOfYear);
+                myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+                updateLabel(edittext, myCalendar);
+            }
+
+        };
+
+        edittext.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                new DatePickerDialog(ItinerarrySearchActivity.this, date, myCalendar
+                        .get(Calendar.YEAR), myCalendar.get(Calendar.MONTH),
+                        myCalendar.get(Calendar.DAY_OF_MONTH)).show();
+            }
+        });
+
     }
+
+    private void updateLabel(EditText edittext, Calendar myCalendar) {
+        String myFormat = getString(R.string.dateDMY); //In which you need put here
+        SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.US);
+
+        edittext.setText(sdf.format(myCalendar.getTime()));
+    }
+
 }
 
 
